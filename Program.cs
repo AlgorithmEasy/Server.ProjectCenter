a528977f43b72a4e16c0dd57c9d9ad20278b4091
SmallY <git@smallyy.cn>
2021-12-01 16:33:51 +0800

    修改项目名称并移除无用代码

--- Program.cs
+++ Program.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Server.ProjectCenter
+namespace AlgorithmEasy.Server.ProjectCenter
 {
     public class Program
     {
